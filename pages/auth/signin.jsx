83d0eff0508adddb9ee11d39d9d3e7eb1bda409f
Scott-fo <scottfo@live.com>
2022-02-09 10:35:51 +0000

    <> to div

--- pages/auth/signin.jsx
+++ pages/auth/signin.jsx
@@ -4,7 +4,7 @@ import Header from '../../components/Header';
 
 function signin({ providers }) {
   return (
-    <>
+    <div>
         <Header />
         <div className='flex flex-col items-center justify-center min-h-screen py-2 -mt-56 px-14 text-center'>
             <img className='w-80' src="https://links.papareact.com/ocw" alt="" />
@@ -23,11 +23,11 @@ function signin({ providers }) {
                 ))}
             </div>
         </div>
-    </>
+    </div>
     );
 }
 
-export async function getServerSideProps(context) {
+export async function getServerSideProps() {
     const providers = await getProviders();
 
     return {
